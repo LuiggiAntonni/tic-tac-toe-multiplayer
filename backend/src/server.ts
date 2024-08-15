b7a7f3f50a446a6c91b88911da01f547fbe9c9a4
Luiggi Antonni <luiggiantonni0129@gmail.com>
2024-08-15 12:21:36 -0300

    Set up backend with Express and Socket.IO

---
+++ backend/src/server.ts
@@ -0,0 +1,22 @@
+import express from 'express';
+import http from 'http';
+import { Server } from 'socket.io';
+
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server);
+
+app.get('/', (req, res) => {
+    res.send('Tic-Tac-Toe Server is running');
+});
+
+io.on('connection', (socket) => {
+    console.log('A user connected');
+    socket.on('disconnect', () => {
+        console.log('User disconnected');
+    });
+});
+
+server.listen(3000, () => {
+    console.log('Server is running on http://localhost:3000');
+});
